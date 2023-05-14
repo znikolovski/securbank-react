3db08b9e9f8b8d24e25babcf38a92dfa085141ea
Mark Szulc <mszulc@adobe.com>
2023-05-14 23:35:39 +1000

    itemId

--- src/components/articles.js
+++ src/components/articles.js
@@ -7,32 +7,22 @@ import { useEffect, useState } from "react"
 
 function Articles() {
 
+
     const [showArticles, setshowArticles] = useState()
     const aemurl = 'https://publish-p55117-e571178.adobeaemcloud.com/graphql/execute.json/frescopa/ArticleList';
     let displayData
 
-    // function fetchCF() {
-    //     fetch(aemurl)
-    //     .then(response => response.json())
-    //     .then(responseData => {
-    //         displayData = responseData.data.articleList.items.map(function(article) {
-    //             return(
-    //                 <p key={article.index}>{article.headline}</p>
-    //             )
-    //         })
-    //         console.log(responseData)
-    //         setshowArticles(displayData)
-    //     })
-    //     return
-    // }
-
     async function fetchCF() {
         const response = await fetch(aemurl)
         const responseData = await response.json()
+        let itemId
+
         console.log(responseData)
         displayData = responseData.data.articleList.items.map(function(article,index) {
+            itemId =  "urn:aemconnection:" + article._path + "/jcr:content/data/master";
+
             return(
-                <li key={index} itemscope="" itemid="urn:aemconnection:undefined/jcr:content/data/master" itemType="reference" itemfilter="cf">
+                <li key={index} itemScope="" itemId={itemId} itemType="reference" itemfilter="cf">
                 <div itemProp="headline" itemType="text">
                     <h5>{article.headline}</h5>
                 </div>
