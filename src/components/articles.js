e20730d7b0d9fd0b0dad8d980a0fc5d42b8dd824
Mark Szulc <mszulc@adobe.com>
2023-05-15 16:49:57 +1000

    add Image Support

--- src/components/articles.js
+++ src/components/articles.js
@@ -7,6 +7,7 @@ import './articles.css';
 function Articles() {
 
     const [showArticles, setshowArticles] = useState()
+    const aempublishurl = 'https://publish-p55117-e571178.adobeaemcloud.com';
     const aemurl = 'https://author-p55117-e571178.adobeaemcloud.com/graphql/execute.json/frescopa/ArticleList'+"?ts="+Math.random()*1000;
     let displayData
     let options = {credentials: "include"};
@@ -14,16 +15,21 @@ function Articles() {
 
     async function fetchCF() {
         const response = await fetch(aemurl,options)
+        // TODO - Add error handling here
         const responseData = await response.json()
-        let itemId
+        // TODO - Add error handling here
+        let itemId, imageURL
+
 
         console.log(responseData)
         displayData = responseData.data.articleList.items.map(function(article,index) {
             itemId =  "urn:aemconnection:" + article._path + "/jcr:content/data/master";
             console.log(itemId)
+            imageURL = aempublishurl + article.heroImage._dynamicUrl + "&width=100";
 
             return(
                 <li key={index} itemScope itemID={itemId} itemType="reference" itemfilter="cf">
+                    <img itemProp="heroImage" itemType="image" src={imageURL} />
                     <h5 itemProp="headline" itemType="text">{article.headline}</h5>
                     <div itemProp="main" itemType="richtext">{article.main['plaintext']}</div>
                 </li>
